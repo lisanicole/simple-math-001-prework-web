820540b7d986f5d200b9429d897ab20e2161a80f
lisanicole <lisanicole67@aol.com>
2015-09-11 12:45:35 -0400

    Math lab done

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,39 @@
 def addition(num1, num2)
+    total_num = num1 + num2
+    puts "#{num1} + #{num2} = #{total_num}"
+    total_num
 end
 
 def subtraction(num1, num2)
+    total_num = num1 - num2
+    puts "#{num1} - #{num2} = #{total_num}"
+    total_num
 end
 
 def division(num1, num2)
+    total_num = num1 / num2
+    puts "#{num1} divided by  #{num2} = #{total_num}"
+    total_num
 end
 
 def multiplication(num1, num2)
+    total_num = num1 * num2
+    puts "#{num1} multiplied by  #{num2} = #{total_num}"
+    total_num
 end
 
 def modulo(num1, num2)
+    total_num = num1 % num2
+    puts "The remainder of #{num1} divided by #{num2} = #{total_num}"
+    total_num
 end
 
 def square_root(num)
+    puts "The square root of #{num} = #{Math.sqrt(num)}"
+    Integer(Math.sqrt(num))
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  total_num =  num1 + (( num2 * num3 ) / num4 )
+  total_num
 end
